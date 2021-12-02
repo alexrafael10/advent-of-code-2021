66f4e6e69543c6c24c4d8a4bd50e770b71863c9b
alex <alexrafael10@gmail.com>
2021-12-02 11:00:42 -0300

    fixed path in part1

--- day-1/part-1/index.js
+++ day-1/part-1/index.js
@@ -3,7 +3,7 @@ const { processLineByLine } = require("../../utils");
 
 
 (async function () {
-  const depthMeasurement = await processLineByLine("day-1/input.txt");
+  const depthMeasurement = await processLineByLine("day-1/part-1/input.txt");
   const { increases } = depthMeasurement.reduce(function (acc, current) {
     const currentMeasurement = parseInt(current);
     if (acc.prev && currentMeasurement > acc.prev) {
