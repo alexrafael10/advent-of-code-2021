c140a63ad6f3ea0978c3bc20a32643db02fcef69
alex <alexrafael10@gmail.com>
2021-12-05 10:58:15 -0300

    tweak day 2 part 1

--- day-2/part-1/index.js
+++ day-2/part-1/index.js
@@ -4,16 +4,13 @@ const { processLineByLine } = require("../../utils");
   const commands = await processLineByLine("day-2/part-1/input.txt");
   const commandsDelta = {
     "up": -1,
-    "down": +1,
-    "forward": +1,
+    "down": +1
   };
-
   const { depth, horizontal } = commands.reduce(
     (acc, commandLine) => {
       const [command, delta] = commandLine.split(" ");
-      const change = commandsDelta[command] * delta;
-      if (command === "forward") acc.horizontal += change;
-      else acc.depth += change;
+      if (command === "forward") acc.horizontal += parseInt(delta);
+      else acc.depth += commandsDelta[command] * parseInt(delta);
       return acc;
     },
     { depth : 0, horizontal : 0 }
